7180ee98b70b47103e59bd7281970eed6c3010b8
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2012-09-11 15:20:16 +0900

    android の出力を softbank の絵文字コードにした

--- lib/jpmobile/mobile/android.rb
+++ lib/jpmobile/mobile/android.rb
@@ -7,6 +7,30 @@ module Jpmobile::Mobile
     # 対応するUser-Agentの正規表現
     USER_AGENT_REGEXP = /Android/
 
-    include Jpmobile::Mobile::GoogleEmoticon
+    # Jpmobile::Rack::Filter を適用する
+    def apply_filter?
+      true
+    end
+
+    # Jpmobile::Rack::ParamsFilter を適用する
+    def apply_params_filter?
+      true
+    end
+
+    # 文字コード変換
+    def to_internal(str)
+      # Google絵文字を数値参照に変換
+      str = Jpmobile::Emoticon.external_to_unicodecr_google(Jpmobile::Util.utf8(str))
+      # 数値参照を UTF-8 に変換
+      Jpmobile::Emoticon.unicodecr_to_utf8(str)
+    end
+    def to_external(str, content_type, charset)
+      # UTF-8を数値参照に
+      str = Jpmobile::Emoticon.utf8_to_unicodecr(str)
+      # 数値参照を絵文字コードに変換
+      str = Jpmobile::Emoticon.unicodecr_to_external(str, Jpmobile::Emoticon::CONVERSION_TABLE_TO_SOFTBANK, false) # mbga のために、必ず softbank のコードを使う
+
+      [str, charset]
+    end
   end
 end
